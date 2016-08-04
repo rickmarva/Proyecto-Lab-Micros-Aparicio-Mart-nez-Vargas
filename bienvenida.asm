8d7b6b591c9d727ac8f548080d41f2dc39eef824
japariciomorales <japaricio.morales@gmail.com>
2016-08-04 15:51:01 -0600

    Add files via upload

---
+++ bienvenida.asm
@@ -0,0 +1,47 @@
+section .data
+	bienvenida: db 0x1b,"[1;1H",0x1b,"[2J","Bienvenidos a Micronoid",0xa
+	bienvenida_tamano: equ $-bienvenida
+
+	curso: db 'EL-4313-Lab. Estructrura de Microprocesadores 2S-2016',0xa
+	curso_tamano: equ $-curso
+
+	msj_ingrese: db 'Ingrese el nombre del jugador: '
+	msj_ingrese_tamano: equ $-msj_ingrese
+
+	borrar: db 0x1b,"[1;1H",0x1b,"[2J",""
+	borrar_tamano: equ $-borrar
+
+	nombre: db '',0xa
+
+section .text
+	global _start
+
+_start:
+	mov rax,1
+	mov rdi,1
+	mov rsi,bienvenida
+	mov rdx,bienvenida_tamano
+	syscall
+	mov rax,1
+        mov rdi,1
+        mov rsi,curso
+        mov rdx,curso_tamano
+	syscall
+	mov rax,1
+	mov rdi,1
+	mov rsi,msj_ingrese
+	mov rdx,msj_ingrese_tamano
+	syscall
+	mov rax,0
+	mov rdi,0
+	mov rsi,nombre
+	mov rdx,30
+	syscall
+	mov rax,1
+	mov rdi,1
+	mov rsi,borrar
+	mov rdx,borrar_tamano
+	syscall
+	mov rax,60
+	mov rdi,0
+	syscall
